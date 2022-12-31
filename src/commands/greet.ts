4309b11588a4c06bfc5e169af4e3cdbce04003ba
Nawapat Buakoet <nawapatb.2b@hotmail.com>
2022-12-31 23:23:20 +0700

    add object `enable` in Command & SlashCommand for optional
    add object `enable` in Command & SlashCommand for optional

--- src/commands/greet.ts
+++ src/commands/greet.ts
@@ -1,15 +1,20 @@
 import { PermissionFlagsBits } from "discord.js";
 import { Command } from "../types";
 
-const command : Command = {
-    name: "greet",
-    execute: (message, args) => {
-        let toGreet = message.mentions.members?.first()
-        message.channel.send(`Hello there ${toGreet ? toGreet.user.username : message.member?.user.username}!`)
-    },
-    cooldown: 10,
-    aliases: ["sayhello"],
-    permissions: ["Administrator", PermissionFlagsBits.ManageEmojisAndStickers] // to test
-}
+const command: Command = {
+  enable: true,
+  name: "greet",
+  execute: (message, args) => {
+    let toGreet = message.mentions.members?.first();
+    message.channel.send(
+      `Hello there ${
+        toGreet ? toGreet.user.username : message.member?.user.username
+      }!`
+    );
+  },
+  cooldown: 10,
+  aliases: ["sayhello"],
+  permissions: ["Administrator", PermissionFlagsBits.ManageEmojisAndStickers], // to test
+};
 
-export default command
+export default command;
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,54 +1,63 @@
-import { SlashCommandBuilder, CommandInteraction, Collection, PermissionResolvable, Message, AutocompleteInteraction } from "discord.js"
-import mongoose from "mongoose"
+import {
+  SlashCommandBuilder,
+  CommandInteraction,
+  Collection,
+  PermissionResolvable,
+  Message,
+  AutocompleteInteraction,
+} from "discord.js";
+import mongoose from "mongoose";
 
 export interface SlashCommand {
-    command: SlashCommandBuilder | any,
-    execute: (interaction : CommandInteraction) => void,
-    autocomplete?: (interaction: AutocompleteInteraction) => void,
-    cooldown?: number // in seconds
+  enable: boolean;
+  command: SlashCommandBuilder | any;
+  execute: (interaction: CommandInteraction) => void;
+  autocomplete?: (interaction: AutocompleteInteraction) => void;
+  cooldown?: number; // in seconds
 }
 
 export interface Command {
-    name: string,
-    execute: (message: Message, args: Array<string>) => void,
-    permissions: Array<PermissionResolvable>,
-    aliases: Array<string>,
-    cooldown?: number,
+  name: string;
+  enable: boolean;
+  execute: (message: Message, args: Array<string>) => void;
+  permissions: Array<PermissionResolvable>;
+  aliases: Array<string>;
+  cooldown?: number;
 }
 
 interface GuildOptions {
-    prefix: string,
+  prefix: string;
 }
 
 export interface IGuild extends mongoose.Document {
-    guildID: string,
-    options: GuildOptions
-    joinedAt: Date
+  guildID: string;
+  options: GuildOptions;
+  joinedAt: Date;
 }
 
-export type GuildOption = keyof GuildOptions
+export type GuildOption = keyof GuildOptions;
 export interface BotEvent {
-    name: string,
-    once?: boolean | false,
-    execute: (...args?) => void
+  name: string;
+  once?: boolean | false;
+  execute: (...args) => void;
 }
 
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            TOKEN: string,
-            CLIENT_ID: string,
-            PREFIX: string,
-            MONGO_URI: string,
-            MONGO_DATABASE_NAME: string
-        }
+  namespace NodeJS {
+    interface ProcessEnv {
+      TOKEN: string;
+      CLIENT_ID: string;
+      PREFIX: string;
+      MONGO_URI: string;
+      MONGO_DATABASE_NAME: string;
     }
+  }
 }
 
 declare module "discord.js" {
-    export interface Client {
-        slashCommands: Collection<string, SlashCommand>
-        commands: Collection<string, Command>,
-        cooldowns: Collection<string, number>
-    }
+  export interface Client {
+    slashCommands: Collection<string, SlashCommand>;
+    commands: Collection<string, Command>;
+    cooldowns: Collection<string, number>;
+  }
 }
